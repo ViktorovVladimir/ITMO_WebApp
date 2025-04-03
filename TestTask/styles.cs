6892847d998ba0ffb66d8a112a729d4b2b478916
v-bat <167399035+ViktorovVladimir@users.noreply.github.com>
2025-04-03 16:53:23 +0300

    Completed task 1.1 of the entire course assignment.
    - Completed task 1.1 of the entire course assignment:
    1. Develop a website of arbitrary content and design containing at least 5 HTML pages and meeting the following requirements (for credit, just select and complete any 5 from the list):
    1.1.	according to the content of the website you have developed, it is necessary to use elements of semantic markup of the HTML5 language (at least 5 different elements);

---
+++ TestTask/styles.cs
@@ -0,0 +1,23 @@
+body {
+    font-family: Arial, sans-serif;
+}
+
+header {
+    background-color: #333;
+    color: white;
+    padding: 10px;
+}
+
+nav ul {
+    list-style-type: none;
+}
+
+nav ul li {
+    display: inline;
+    margin-right: 15px;
+}
+
+footer {
+    text-align: center;
+    margin-top: 20px;
+}
